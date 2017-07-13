9809e32785d6e9ce932e7111f29ef125f9d3599b
MongCheng <mongcheng@gmail.com>
2017-07-13 09:50:04 +0800

    Update endpoint URL

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ import uuid
 
 app = Flask(__name__)
 VERSION = 'v0.1.1'
-CC_BOT_ENDPOINT = 'http://13.113.84.243'
+CC_BOT_ENDPOINT = 'http://lineage.twbbs.org'
 DEFAULT_HEADERS = {'Content-Type': 'application/json'}
 HELP_FILE_PATH = 'cmd_help.txt'
 HELP_MSG = None
